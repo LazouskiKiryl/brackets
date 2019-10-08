5dd34cf0c41443937435bd1f4012e00a5a40ad43
LazouskiKiryl <kiryllazouski@gmail.com>
2019-10-08 21:14:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -18,36 +18,3 @@ module.exports = function check(str, bracketsConfig) {
         lengthStr = str.length;
     }
 }
-
-// //   const st = '([{}])';
-// //   const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-
-//   let openBrackets = [];
-//   let closeBrackets = [];
-
-//   for (let pairBrackets of bracketsConfig) {
-//     openBrackets.push(pairBrackets[0]);
-//     closeBrackets.push(pairBrackets[1]);
-//   }
-
-
-//   let stack = [];
-//   isOpenBracket('a');
-
-//   for (let bracket of str) {
-//     let index = closeBrackets.indexOf(bracket);
-    
-
-//     if (index === -1) {
-//       stack.push(bracket);
-//     } else  if (stack.pop() !== openBrackets[index]) {
-//       return false;
-//     }
-
-//   }
-
-//   return true;
-
-//   let isOpenBracket = (br) => {
-
-//   }
