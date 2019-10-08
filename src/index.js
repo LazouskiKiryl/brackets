e0354dd3f33ea1b37e8868977442664f6a583228
LazouskiKiryl <kiryllazouski@gmail.com>
2019-10-08 20:54:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let pairsBrackets = [];
+    let lengthStr = str.length;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        pairsBrackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+
+    while (true) {
+        for (let i = 0; i < pairsBrackets.length; i++) {
+            str = str.replace(pairsBrackets[i], '');
+        }
+
+        if (str.length == 0) return true;
+        if (lengthStr == str.length) return false;
+
+        lengthStr = str.length;
+    }
 }
+
+// //   const st = '([{}])';
+// //   const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+
+//   let openBrackets = [];
+//   let closeBrackets = [];
+
+//   for (let pairBrackets of bracketsConfig) {
+//     openBrackets.push(pairBrackets[0]);
+//     closeBrackets.push(pairBrackets[1]);
+//   }
+
+
+//   let stack = [];
+//   isOpenBracket('a');
+
+//   for (let bracket of str) {
+//     let index = closeBrackets.indexOf(bracket);
+    
+
+//     if (index === -1) {
+//       stack.push(bracket);
+//     } else  if (stack.pop() !== openBrackets[index]) {
+//       return false;
+//     }
+
+//   }
+
+//   return true;
+
+//   let isOpenBracket = (br) => {
+
+//   }
